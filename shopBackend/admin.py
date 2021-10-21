37df73d13d5f10382f8e1615ad005d8dd90634bd
Paritosh_Kabra <paritosh_k@cs.iitr.ac.in>
2021-10-21 23:03:58 +0530

    Google Oauth implemented

---
+++ shopBackend/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+
+# Register your models here.
+# 194665914760-o3v6gu9gn706vkkt395hauo3sm7vuh96.apps.googleusercontent.com
---
+++ shopBackend/views.py
@@ -0,0 +1,8 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def google_oauth(request):
+    return HttpResponse("Oauth will be implemented soon!")
